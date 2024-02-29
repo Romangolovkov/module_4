82a1cd2bb75c9e0a64df4f5c7dac47f4e7c0adca
Roman Golovkov <romangolovkov@mail.ru>
2024-02-29 14:42:49 +0800

    lesson 2 solution 1 corrected

--- lesson_2/src/solution_1.py
+++ lesson_2/src/solution_1.py
@@ -17,14 +17,14 @@ sales: dict = {
   ]
 }
 
-
-def calculation_revenue(dct: dict) -> None:
-    if "category" in dct and "quantity" in dct and "total_price" in dct:
-        print(f'Выручка по категории "{dct["category"]}": {dct["quantity"] * dct["total_price"]}')
-
-
 with open('sales.json', 'w', encoding='utf8') as file:
     json.dump(sales, file)
+
+
+def calculation_revenue(dct: dict[str: str|int]) -> None:
+    if "category" in dct and "quantity" in dct and "total_price" in dct:
+        print(f'Выручка по категории "{dct["category"]}": {dct["quantity"] * dct["total_price"]}')
+        
 
 with open('sales.json', 'r', encoding='utf8') as file:
     json.load(file, object_hook=calculation_revenue)

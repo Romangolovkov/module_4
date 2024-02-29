73e23f36daf78791e7ea19dc3a874df32c95b2ad
Roman Golovkov <romangolovkov@mail.ru>
2024-02-29 14:50:17 +0800

    lesson 2 solution 2

---
+++ lesson_2/src/solution_2.py
@@ -0,0 +1,31 @@
+import json
+
+inventory: dict = {
+  "inventory": [
+    {
+      "item": "Кисти",
+      "category": "Инструменты",
+      "quantity": 5,
+      "minimum_required": 10
+    },
+    {
+      "item": "Краска акриловая",
+      "category": "Материалы",
+      "quantity": 20,
+      "minimum_required": 150
+    }
+  ]
+}
+
+with open('inventory.json', 'w', encoding='utf8') as file:
+    json.dump(inventory, file)
+
+
+def inventory_check(dct: dict[str: str|int]) -> None:
+    if "item" in dct and "quantity" in dct and "minimum_required" in dct:
+        if dct["quantity"] < dct["minimum_required"]:
+            print(f'Необходимо закупить: {dct["item"]} {dct["minimum_required"] - dct["quantity"]} шт.')
+
+
+with open('inventory.json', 'r', encoding='utf8') as file:
+    json.load(file, object_hook=inventory_check)
